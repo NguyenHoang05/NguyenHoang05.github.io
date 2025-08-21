986d89f97460b56a51cc80c6f2bf941bdd58d5a7
Mrcucusi <vandongbui1512@gmail.com>
2025-08-21 20:23:30 +0700

    embedded chatbot

--- chatbotAI/script.js
+++ chatbotAI/script.js
@@ -1,4 +1,22 @@
-import { db } from "./firebase-config.js";
+// firebase-config.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAGA76Ts_51KAFItYa-HrwV26gHxbp69Ws",
+  authDomain: "mybiglibrary-f9988.firebaseapp.com",
+  projectId: "mybiglibrary-f9988",
+  storageBucket: "mybiglibrary-f9988.firebasestorage.app",
+  messagingSenderId: "841203291050",
+  appId: "1:841203291050:web:1e061841630d5b5d63d1d3",
+  measurementId: "G-BBKVYTGTWY"
+};
+// Khởi tạo Firebase App
+const app = initializeApp(firebaseConfig);
+// Xuất đối tượng Firestore ra ngoài để file khác dùng
+const db = getFirestore(app);
+
 import { collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const chatBody = document.querySelector(".chat-body");
