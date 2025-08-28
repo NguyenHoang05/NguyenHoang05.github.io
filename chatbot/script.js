5a2763c6d9e1fb25d2a4f0db6df73ff19c843f4d
NguyenHoang05 <nguyenhoang08052004@gmail.com>
2025-08-28 20:01:40 +0700

    test

--- chatbot/script.js
+++ chatbot/script.js
@@ -219,7 +219,7 @@ async function getGeminiResponseCached(message) {
 }
 
 async function sendMessageToBot(message) {
-  const response = await fetch('http://localhost:3000/chat', {
+  const response = await fetch('https://nguyenhoang05-github-io.onrender.com/api/chat', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ message })
